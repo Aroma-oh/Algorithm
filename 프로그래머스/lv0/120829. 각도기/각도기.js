8c32b33f3115a0d365149d6a2ac4a4797d1127a1
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-01-28 12:39:37 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,6 +1,3 @@
 function solution(angle) {
-    if (angle === 180) return 4;
-    else if (angle === 90) return 2;
-    else if (0 < angle && angle < 90) return 1;
-    else if (90 < angle && angle < 180) return 3;
+    return [1, 90, 91, 180].filter(x => angle>=x).length;
 }
