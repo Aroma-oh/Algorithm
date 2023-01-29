2fcdb5b32ef88e7cf25f3e40fd3a44b73d0d3e78
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-01-29 12:20:38 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,8 +1,4 @@
 function solution(emergency) {
     const sorted = [...emergency].sort((a,b) => b-a);
-    const result = new Array(emergency.length).fill(0);
-    for (let i = 0; i < emergency.length; i++) {
-        result[i] = sorted.indexOf(emergency[i]) + 1;
-    }
-    return result;
+    return emergency.map(el => sorted.indexOf(el)+1);
 }
