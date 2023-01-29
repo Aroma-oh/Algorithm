a24ee733021f6ba9b53d5af2a91bde94659bdb85
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-01-29 12:40:44 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -3,8 +3,7 @@
 function solution(n) {
     let count = 0;
     for (let i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) count++;
+        if (n % i === 0) count += 2;
     }
-    if (Number.isInteger(Math.sqrt(n))) return count*2-1;
-    else return count*2;
+    return Number.isInteger(Math.sqrt(n)) ? count-1 : count;
 }
