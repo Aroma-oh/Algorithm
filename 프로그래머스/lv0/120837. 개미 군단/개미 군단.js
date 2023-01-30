4714e347f42abc366852a9cd5b7a5b516cbd2a3e
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-01-30 09:38:42 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,15 +1,3 @@
 function solution(hp) {
-    let count = 0; 
-    
-    while (count <= hp) {
-        count = parseInt(hp/5); 
-        if (hp%5 !== 0) {
-            count += parseInt(hp%5/3);
-            if (hp%5/3 !== 0){
-                count += parseInt(hp%5%3);
-                break; 
-            } else break; 
-        } else break; 
-    }
-    return count;
+    return parseInt(hp/5) + parseInt(hp%5/3) + (hp%5%3)
 }
