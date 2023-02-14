545f99ef85820b5dca8732b30a7aa616011a347c
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-02-14 21:35:58 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,20 +1,4 @@
 function solution(sides) {
-    let maxIdx = sides.indexOf(Math.max(...sides));
-    let added = 0; 
-    for (let i = 0; i < sides.length; i++) {
-        if (i !== maxIdx) added += sides[i];
-    }
-    return Math.max(...sides) < added ? 1 : 2; 
+    sides.sort((a, b) => a-b); 
+    return (sides[0] + sides[1]) > sides[2] ? 1 : 2;
 }
-
-// 런타임 에러
-// let max = Math.max(...sides);
-// let added = sides.filter(el => el !== max).reduce((a, b) => a+b);
-// return added > max ? 1 : 2;
-
-    // let maxSide = sides[0]; 
-    // let added = 0; 
-    // for (let i = 1; i < sides.length; i++) {
-    //     if (sides[i] > maxSide) maxSide = sides[i]; 
-    //     else added +=
-    // }
