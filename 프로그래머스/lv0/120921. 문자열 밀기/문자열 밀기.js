7546a3b2dc424366f68745346893b67a507417eb
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-02-28 22:32:12 +0900

    [level 0] Title: 문자열 밀기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,1 +1,10 @@
-let solution = (a, b) => (b+b).indexOf(a)
+function solution(A, B) {
+    let newArr = [...A];
+    for ( i = 0; i < A.length; i ++) {
+        if (newArr.join("") === B) return i;
+        else {
+            newArr.unshift(newArr.pop());
+        }
+    }
+    return -1;
+}
